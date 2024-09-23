964ab4d7e0715dcdad7285db41cf3d0c9869b838
Unity2033 <kileab@naver.com>
2024-09-23 21:44:54 +0900

    (2024.9.23) 마우스 시스템

--- Program/Source.c
+++ Program/Source.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <windows.h>
+
+void MouseClick(int* x, int* y)
+{
+	HANDLE       hIn, hOut;
+	DWORD        dwNOER;
+	INPUT_RECORD rec;
+
+	hIn = GetStdHandle(STD_INPUT_HANDLE);
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleMode(hIn, ENABLE_PROCESSED_INPUT | ENABLE_MOUSE_INPUT);
+
+	while (TRUE) {
+		ReadConsoleInput(hIn, &rec, 1, &dwNOER);
+
+		if (rec.EventType == MOUSE_EVENT) {
+			if (rec.Event.MouseEvent.dwButtonState & FROM_LEFT_1ST_BUTTON_PRESSED) {
+				*x = rec.Event.MouseEvent.dwMousePosition.X;
+				*y = rec.Event.MouseEvent.dwMousePosition.Y;
+				return;
+			}
+		}
+	}
+}
 
 int main()
 {
+	int x, y;
 
-	return 0;
+	printf("³¡³»±â");
+
+	while (1) {
+		MouseClick(&x, &y);
+		if (y == 0) {
+			if (x < 6) break;
+		}
+
+		printf("%2d, %2d\n", x, y);
+	}
 }
