20a48d090f3e3f71d2b4c25378ebdd330437af50
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-12-06 15:32:35 +0900

    (2024.12.6) 시간 함수

--- Program/Source.c
+++ Program/Source.c
@@ -1,41 +1,110 @@
 #include <stdio.h>
 #include <windows.h>
+#include <conio.h>
 
-void MouseClick(int* x, int* y)
+#define WIDTH 11
+#define HEIGHT 11
+
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+typedef struct Character
 {
-	HANDLE       hIn, hOut;
-	DWORD        dwNOER;
-	INPUT_RECORD rec;
+	int x;
+	int y;
+	const char* shape;
+}Character;
 
-	hIn = GetStdHandle(STD_INPUT_HANDLE);
-	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleMode(hIn, ENABLE_PROCESSED_INPUT | ENABLE_MOUSE_INPUT);
+char maze[HEIGHT][WIDTH] =
+{
+	{'1','1','1','1','1','1','1','1','1','1','1'},
+	{'1','0','0','0','0','1','1','1','1','2','1'},
+	{'1','1','0','1','0','0','1','1','1','0','1'},
+	{'1','1','0','1','1','0','0','1','1','0','1'},
+	{'1','1','0','0','1','1','0','0','1','0','1'},
+	{'1','1','1','0','1','1','1','0','1','0','1'},
+	{'1','1','1','0','0','0','0','1','1','0','1'},
+	{'1','1','1','0','0','1','0','1','1','0','1'},
+	{'1','1','1','1','1','1','0','1','1','0','1'},
+	{'1','1','0','0','0','0','0','0','0','0','1'},
+	{'1','1','1','1','1','1','1','1','1','1','1'},
+};
 
-	while (TRUE) {
-		ReadConsoleInput(hIn, &rec, 1, &dwNOER);
-
-		if (rec.EventType == MOUSE_EVENT) {
-			if (rec.Event.MouseEvent.dwButtonState & FROM_LEFT_1ST_BUTTON_PRESSED) {
-				*x = rec.Event.MouseEvent.dwMousePosition.X;
-				*y = rec.Event.MouseEvent.dwMousePosition.Y;
-				return;
+void Render()
+{
+	for (int i = 0; i < HEIGHT; i++)
+	{
+		for (int j = 0; j < WIDTH; j++)
+		{
+			if (maze[i][j] == '0')
+			{
+				printf("  ");
+			}
+			else if (maze[i][j] == '1')
+			{
+				printf("■");
+			}
+			else
+			{
+				printf("⊙");
 			}
 		}
+
+		printf("\n");
 	}
+}
+
+void Position(int x, int y)
+{
+	// X와 Y축을 설정하는 구조체입니다.
+	COORD position = { x, y };
+
+	// 콘솔 커서의 좌표를 설정하는 함수입니다.
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
 }
 
 int main()
 {
-	int x, y;
+	Character character = { 2, 1, "★" };
 
-	printf("끝내기");
+	char key = 0;
 
-	while (1) {
-		MouseClick(&x, &y);
-		if (y == 0) {
-			if (x < 6) break;
+	Render();
+
+	while (1)
+	{
+		if (_kbhit())
+		{
+			key = _getch();
+
+			if (key == -32)
+			{
+				key = _getch();
+			}
+
+			switch (key)
+			{
+			case UP: if (maze[character.y - 1][character.x / 2] != '1') character.y--;
+				break;
+			case LEFT: if (maze[character.y][character.x / 2 - 1] != '1') character.x -= 2;
+				break;
+			case RIGHT: if (maze[character.y][character.x / 2 + 1] != '1') character.x += 2;
+				break;
+			case DOWN: if (maze[character.y + 1][character.x / 2] != '1') character.y++;
+				break;
+			}
+
+			system("cls");
+
+			Render();
 		}
 
-		printf("%2d, %2d\n", x, y);
+		Position(character.x, character.y);
+		printf("%s", character.shape);
 	}
+
+
+	return 0;
 }
